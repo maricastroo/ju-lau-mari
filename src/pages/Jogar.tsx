c515d57aae9c00976768aeddb687d1a8fa1a091f
maricastroo <castromariana05g@gmail.com>
2025-05-24 01:10:45 -0300

    Atualização das perguntas e respostas do jogo

--- src/pages/Jogar.tsx
+++ src/pages/Jogar.tsx
@@ -9,39 +9,48 @@ import { Link } from 'react-router-dom';
 const quizData = [
   {
     id: 1,
-    caso: "caso 1",
+    caso: "Durante a pandemia de covid-19 houve, por consequência da Medida Provisória nº 954, de 17 de abril de 2020, o compartilhamento de dados pessoais por empresas de telecomunicações com o IBGE. ", 
+    
     opcoes: [
-      "opção 1",
-      "opção 2",
-      "opção 3",
-      "opção 4"
-    ],
-    respostaCorretaIndex: 1,
-    explicacao: "essa é a resposta correta porque..."
-  },
-  {
-    id: 2,
-    caso: "caso 2",
-    opcoes: [
-      "opção 1",
-      "opção 2",
-      "opção 3",
-      "opção 4"
+      "Sigilo de dados (privacidade) ",
+      " Estatísticas (Informação)"
     ],
     respostaCorretaIndex: 0,
-    explicacao: "essa é a resposta correta porque..."
+    explicacao: "As operadoras telefônicas não poderiam ser obrigadas a compartilhar dados de usuários durante a pandemia de COVID-19, mesmo que para fins de suporte à produção estatística oficial durante a situação de emergência de saúde pública de importância internacional. Tal compartilhamento  deve ser limitado ao mínimo necessário para não violar o direito ao sigilo de dados, à intimidade, à vida privada, e à autodeterminação informativa. ",
+  
   },
+
+  {
+    id: 2,
+    caso: "Caso Francischini: No dia das eleições de 2018, o então deputado federal Fernando Francischini (PSL-PR) realizou uma live alegando, sem provas, que urnas eletrônicas estavam fraudadas para impedir votos no candidato Jair Bolsonaro. O Tribunal Superior Eleitoral (TSE), em outubro de 2021, cassou o mandato de Francischini por uso indevido dos meios de comunicação e abuso de poder político, tornando-o inelegível por oito anos. Em junho de 2022, o ministro Kassio Nunes Marques, do STF, concedeu liminar suspendendo os efeitos da decisão do TSE, permitindo que Francischini retornasse ao cargo. Entretanto, poucos dias depois, a Segunda Turma do STF, por maioria de 3 votos a 2, restabeleceu a cassação e inelegibilidade do deputado.",
+    opcoes: [
+      "liberdade de expressão",
+      "integridade do processo eleitoral (proteção da democracia)"
+    ],
+    respostaCorretaIndex: 1,
+    explicacao: "Consolidou o entendimento de que a disseminação de fake news por agentes públicos, especialmente em contextos eleitorais, pode configurar abuso de poder e justificar sanções severas, incluindo cassação de mandato e inelegibilidade. Jurisprudência: TUTELA PROVISÓRIA ANTECEDENTE 39",
+  },
+
   {
     id: 3,
-    caso: "caso 3",
+    caso: "Caso Luth (1958): Erich Lüth, jornalista, convocou publicamente um boicote ao filme de Veit Harlan, cineasta envolvido com o regime nazista. A empresa de produção moveu ação civil para impedir a campanha, alegando violação do § 826 do Código Civil Alemão (ato contrário aos bons costumes).",
     opcoes: [
-      "opção 1",
-      "opção 2",
-      "opção 3",
-      "opção 4"
+      "Direito ao boicote (liberdade de expressão)",
+      "Liberdade profissional e econômica"
     ],
-    respostaCorretaIndex: 2,
-    explicacao: "essa é a resposta correta porque..."
+    respostaCorretaIndex: 0,
+    explicacao: "O Tribunal Constitucional declarou que a liberdade de expressão deve ser protegida mesmo nas relações entre particulares, reconhecendo a eficácia horizontal dos direitos fundamentais (Drittwirkung). O Estado, ao aplicar normas civis, deve considerar os valores constitucionais — os direitos fundamentais são elementos estruturantes da ordem jurídica como um todo."
+  },
+
+  {
+    id: 4,
+    caso: "caso 4",
+    opcoes: [
+      "liberdade de expressão",
+      "proteção da ordem pública"
+    ],
+    respostaCorretaIndex: 0,
+    explicacao: "O  STF entendeu que as plataformas digitais têm responsabilidade sobre os conteúdos publicados e disseminados pela rede, por serem capazes de moderar adequadamente esse conteúdo. Portanto, estarão sujeitas às medidas restritivas (espécie de sanção/punição) quando falham e não coíbem atos considerados criminosos em suas plataformas."
   },
 ];
 
@@ -107,12 +116,9 @@ export default function Jogar() {
         {/* conteúdo do texto do banner do quiz*/}
         <div className="max-w-4xl mx-auto px-8 text-center relative z-10"> 
           <h1 className="text-4xl md:text-5xl font-extrabold leading-tight mb-6 animate-fade-in-up">
-            Jogo <span className="text-cyan-300">Quiz</span>
-            <br /> sobre Constitucionalismo Digital
+            Jogo <span className="text-cyan-300">Constitucional</span>
+            <br /> 
           </h1>
-          <p className="text-xl md:text-2xl text-blue-100 font-light mb-8 opacity-0 animate-fade-in delay-200">
-            Responda às perguntas e aprofunde-se no tema!
-          </p>
         </div>
       </section>
 
