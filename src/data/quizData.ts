5fc75596cea77f982d78ad27e5e82040f65a1eab
kyoulau <laucosan05@gmail.com>
2025-06-02 19:18:08 -0300

    fix: new questions

--- src/data/quizData.ts
+++ src/data/quizData.ts
@@ -4,8 +4,11 @@ export const quizData = [
     caso: "Durante a pandemia de covid-19 houve, por consequência da Medida Provisória nº 954, de 17 de abril de 2020, o compartilhamento de dados pessoais por empresas de telecomunicações com o IBGE. ", 
     
     opcoes: [
-      "Sigilo de dados (privacidade) ",
-      " Estatísticas (Informação)"
+      "Sigilo de dados (privacidade) VS. Estatísticas (Informação)",
+      "Vida VS. Saúde pública",
+      "Ordem econômica e financeira VS. família",
+      "Segurança VS. Liberdade de expressão"
+
     ],
     respostaCorretaIndex: 0,
     explicacao: "As operadoras telefônicas não poderiam ser obrigadas a compartilhar dados de usuários durante a pandemia de COVID-19, mesmo que para fins de suporte à produção estatística oficial durante a situação de emergência de saúde pública de importância internacional. Tal compartilhamento  deve ser limitado ao mínimo necessário para não violar o direito ao sigilo de dados, à intimidade, à vida privada, e à autodeterminação informativa. ",
@@ -16,19 +19,23 @@ export const quizData = [
     id: 2,
     caso: "Caso Francischini: No dia das eleições de 2018, o então deputado federal Fernando Francischini (PSL-PR) realizou uma live alegando, sem provas, que urnas eletrônicas estavam fraudadas para impedir votos no candidato Jair Bolsonaro. O Tribunal Superior Eleitoral (TSE), em outubro de 2021, cassou o mandato de Francischini por uso indevido dos meios de comunicação e abuso de poder político, tornando-o inelegível por oito anos. Em junho de 2022, o ministro Kassio Nunes Marques, do STF, concedeu liminar suspendendo os efeitos da decisão do TSE, permitindo que Francischini retornasse ao cargo. Entretanto, poucos dias depois, a Segunda Turma do STF, por maioria de 3 votos a 2, restabeleceu a cassação e inelegibilidade do deputado.",
     opcoes: [
-      "liberdade de expressão",
-      "integridade do processo eleitoral (proteção da democracia)"
+      "Autonomia da vontade VS. Federalismo",
+      "Liberdade de expressão VS. Integridade do processo eleitoral (proteção da democracia)",
+      "Honra VS. Administração Pública",
+      "Interesses internacionais VS. Democracia"
     ],
     respostaCorretaIndex: 1,
-    explicacao: "Consolidou o entendimento de que a disseminação de fake news por agentes públicos, especialmente em contextos eleitorais, pode configurar abuso de poder e justificar sanções severas, incluindo cassação de mandato e inelegibilidade. Jurisprudência: TUTELA PROVISÓRIA ANTECEDENTE 39",
+    explicacao: " A integridade do processo eleitoral ganhou nesse caso, pelo STF entender que a disseminação de fake news por agentes públicos, especialmente em contextos eleitorais, pode configurar abuso de poder e justificar sanções severas, incluindo cassação de mandato e inelegibilidade.",
   },
 
   {
     id: 3,
-    caso: "Caso Luth (1958): Erich Lüth, jornalista, convocou publicamente um boicote ao filme de Veit Harlan, cineasta envolvido com o regime nazista. A empresa de produção moveu ação civil para impedir a campanha, alegando violação do § 826 do Código Civil Alemão (ato contrário aos bons costumes).",
+    caso: "Caso Luth (1958): Erich Lüth, jornalista, convocou publicamente um boicote ao filme de Veit Harlan, cineasta envolvido com o regime nazista. A produtora do filme moveu uma ação civil para impedir a campanha, alegando violação aos bons costumes (§ 826 do Código Civil Alemão).",
     opcoes: [
-      "Direito ao boicote (liberdade de expressão)",
-      "Liberdade profissional e econômica"
+      "Direito ao boicote (liberdade de expressão) VS. Liberdade profissional e econômica",
+      "Direito ao boicote (liberdade de expressão) VS. Propriedade",
+      "Direito ao boicote (liberdade de expressão) VS. Pluralismo político",
+      "Direito ao boicote (liberdade de expressão) VS. Liberdade profissional e econômica"
     ],
     respostaCorretaIndex: 0,
     explicacao: "O Tribunal Constitucional declarou que a liberdade de expressão deve ser protegida mesmo nas relações entre particulares, reconhecendo a eficácia horizontal dos direitos fundamentais (Drittwirkung). O Estado, ao aplicar normas civis, deve considerar os valores constitucionais — os direitos fundamentais são elementos estruturantes da ordem jurídica como um todo."
@@ -36,12 +43,53 @@ export const quizData = [
 
   {
     id: 4,
-    caso: "caso 4",
+    caso: "ADI 5545 – Banco genético - A Lei n. 3.990, de 11 de outubro de 2002, do Estado do Rio de Janeiro, impunha a obrigatoriedade da coleta de material genético de mães e recém-nascidos em hospitais, casas de saúde e maternidades, públicos ou privados, com o objetivo de permitir a identificação posterior através de exame de DNA comparativo em casos de dúvida",
     opcoes: [
-      "liberdade de expressão",
-      "proteção da ordem pública"
+      "Dignidade VS. Soberania",
+      "Cidadania VS. Direito à família",
+      "Privacidade e intimidade VS. Autonomia estadual e Interferência governamental",
+      "Saúde VS. Segurança"
     ],
-    respostaCorretaIndex: 0,
-    explicacao: "O  STF entendeu que as plataformas digitais têm responsabilidade sobre os conteúdos publicados e disseminados pela rede, por serem capazes de moderar adequadamente esse conteúdo. Portanto, estarão sujeitas às medidas restritivas (espécie de sanção/punição) quando falham e não coíbem atos considerados criminosos em suas plataformas."
+    respostaCorretaIndex: 2,
+    explicacao: "Fundamentos da Decisão (Voto do Relator e acompanhamento de Ministros): O Ministro Relator Luiz Fux e outros Ministros (André Mendonça, Nunes Marques, Dias Toffoli, Alexandre de Moraes e Roberto Barroso) votaram pela procedência da ação para declarar a inconstitucionalidade dos artigos impugnados. Os fundamentos incluíram a violação dos princípios da privacidade, intimidade e devido processo legal substantivo. Considerou-se que a medida restritiva de direitos (coleta obrigatória e arquivamento de material genético) não se mostrava estritamente necessária ou adequada, acarretando ônus inúteis. (Filtros Constitucionais - Princípio da Proporcionalidade). Além disso, foi apontada a omissão da lei quanto a salvaguardas e gerenciamento dos dados, implicando vulnerabilidade do sigilo das informações coletadas. A tese proposta foi que 'É inconstitucional a lei estadual que preveja o arquivamento de materiais genéticos de nascituros e parturientes, em unidades de saúde, com o fim de realizar exames de DNA comparativo em caso de dúvida'. Referências a declarações internacionais sobre dados genéticos que exigem consentimento prévio foram utilizadas para nortear a decisão."
+  },
+
+  {
+    id: 5,
+    caso: "ADI 6387 a 6393 – Compartilhamento de dados na pandemia Síntese dos Fatos: Diversas Ações Diretas de Inconstitucionalidade foram ajuizadas (pelo Conselho Federal da OAB e partidos políticos como PSDB, PSB, PSOL, PCdoB) questionando a validade da Medida Provisória (MP) nº 954 de 2020, que determinava que empresas de telefonia disponibilizassem dados de usuários (nomes, números de telefone fixo e móvel, endereços) ao Instituto Brasileiro de Geografia e Estatística (IBGE). O IBGE usaria esses dados para produção de estatísticas oficiais. O contexto é o da pandemia e 'períodos de crise'",
+    opcoes: [
+      "Privacidade VS. Saúde Pública",
+      "Intimidade VS. Soberania Nacional",
+      "Autodeterminação informativa VS. Liberdade",
+      "Inviolabilidade de dados VS. Informação"
+    ],
+    respostaCorretaIndex: 3,
+    explicacao: "A MP foi atacada por violar preceitos fundamentais relacionados à inviolabilidade de dados, privacidade, intimidade, sigilo de dados, e o direito fundamental à autodeterminação informativa. Argumentou-se que a medida violava o princípio da proporcionalidade, em seus sub-princípios de adequação, necessidade e justa medida, por não delimitar o objeto da estatística, a finalidade específica ou sua amplitude, nem esclarecer a necessidade da disponibilização ou como os dados seriam usados. Também houve preocupação com a segurança dos dados devido à estrutura de cargos em comissão do IBGE. Os direitos fundamentais à privacidade e sigilo de dados foram considerados imprescindíveis, mas relativos, exigindo análise de adequação, razoabilidade e proporcionalidade para sua relativização excepcional."
+  },
+
+  {
+    id: 6,
+    caso: "ADPF 186 Síntese dos Fatos: A Arguição de Descumprimento de Preceito Fundamental (ADPF) questionou atos administrativos e normativos da Universidade de Brasília (UnB), incluindo Resolução e Edital de Vestibular, que estabeleceram um sistema temporário de cotas. Esse sistema, segunda a UnB foi implementado com o objetivo de integração Social, Étnica e Racial, considerando as estatísticas sobre a desvantagem sócio-econômica de brasileiros de cor negra comparativamente aos brancos, a Convenção sobre a Eliminação de Todas as Formas de Discriminação Racial, e o uso do fenótipo como critério.",
+    opcoes: [
+      "Princípio Republicano VS. Saúde",
+      "Igualdade VS. Igualdade",
+      "Dignidade da pessoa humana VS. Integridade física e psíquica",
+      "Direito à educação VS. Princípio republicano"
+    ],
+    respostaCorretaIndex: 1,
+    explicacao: "O autor da ação alegou ofensa ao princípio republicano (art. 1º, caput), dignidade da pessoa humana (art. 1º, III), vedação do preconceito de cor e discriminação (art. 3º, IV), repúdio ao racismo (art. 4º, III), e igualdade (art. 5º, I). Os arguidos defenderam a constitucionalidade com base no princípio da dignidade da pessoa humana e a compreensão das políticas de ação afirmativa como deveres extraídos de princípios constitucionais para combater a discriminação e promover a igualdade."
+  },
+
+  {
+    id: 7,
+    caso: "ADPF 54 – Aborto de Feto Anencéfalo Síntese dos Fatos: A Arguição de Descumprimento de Preceito Fundamental (ADPF) foi ajuizada pela Confederação Nacional dos Trabalhadores na Saúde (CNTS) questionando a interpretação dos artigos do Código Penal (124, 126 e 128, I e II) que criminalizam o aborto, no sentido de que essa criminalização alcançaria a antecipação terapêutica do parto de feto anencéfalo. O caso surgiu de decisões judiciais conflitantes sobre a legalidade desse procedimento. A ação pedia a declaração de inconstitucionalidade dessa interpretação, a fim de descriminalizar o aborto em casos de anencefalia.",
+    opcoes: [
+      "Proteção à vida VS. Intimidade",
+      "Proteção à vida VS. Laicidade do Estado",
+      "Proteção à vida VS. Autonomia da vontade",
+      "Proteção à vida VS. Honra"
+    ],
+    respostaCorretaIndex: 2,
+    explicacao: "O cerne da colisão se deu entre a proteção à vida (em tese, desde a concepção) e os direitos fundamentais da gestante: dignidade da pessoa humana (art. 1º, IV), legalidade, liberdade e autonomia da vontade (art. 5º, II), e saúde (art. 6º, caput, e 196). Argumentou-se pela atipicidade do ato (considerando a anencefalia como incompatível com a vida fora do útero), a necessidade de uma interpretação evolutiva do Código Penal de 1940, e a prevalência dos princípios constitucionais da dignidade e saúde."
   },
 ];
